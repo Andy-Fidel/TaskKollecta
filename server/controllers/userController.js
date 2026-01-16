a3afc47a72b5cf54f4af9d5be47e49e1f2eeb78f
Andy-Fidel <andyfidelsam@gmail.com>
2026-01-16 18:08:24 +0000

    Updated password hashing conflict

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -12,24 +12,17 @@ const registerUser = async (req, res) => {
   const { name, email, password } = req.body;
 
   try {
-    
     const userExists = await User.findOne({ email });
     if (userExists) {
       return res.status(400).json({ message: 'User already exists' });
     }
 
-    
-    const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(password, salt);
-
-    
     const user = await User.create({
       name,
       email,
-      password: hashedPassword,
+      password, 
     });
 
-    
     if (user) {
       res.status(201).json({
         _id: user.id,
