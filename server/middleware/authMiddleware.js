d8237b4231c153bb8e1b8131a65183367332870f
Andy-Fidel <andyfidelsam@gmail.com>
2026-01-16 13:04:35 +0000

    Updated server.js

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -5,22 +5,22 @@ const User = require('../models/User');
 const protect = async (req, res, next) => {
   let token;
 
-  // 1. Check if the header starts with "Bearer"
+  
   if (
     req.headers.authorization &&
     req.headers.authorization.startsWith('Bearer')
   ) {
     try {
-      // 2. Get the token from the header (remove "Bearer " string)
+      
       token = req.headers.authorization.split(' ')[1];
 
-      // 3. Verify token
+      
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-      // 4. Get user from the ID in the token (exclude password)
-      req.user = await User.findById(decoded.id).select('-password');
+      
+      req.user = await User.findById(decoded.userId).select('-password');
 
-      next(); // Move to the next function (the controller)
+      next(); 
     } catch (error) {
       console.error(error);
       res.status(401).json({ message: 'Not authorized, token failed' });
